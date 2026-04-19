b0d46af0101c627b0301ef29a51b5d23ada4ec4b
soykantekin <tekin.soykan@ibu.edu.mk>
2026-04-19 13:46:35 +0000

    Sun, Apr 19, 2026, 3:46 PM +02:00

---
+++ week5/speller/dictionary.h
@@ -0,0 +1,16 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <stdbool.h>
+
+// Maximum length for a word
+#define LENGTH 45
+
+// Function prototypes
+bool check(const char *word);
+unsigned int hash(const char *word);
+bool load(const char *dictionary);
+unsigned int size(void);
+bool unload(void);
+
+#endif // DICTIONARY_H
